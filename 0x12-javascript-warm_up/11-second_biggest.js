8186f89d2896dca24130089efb8448fdc4e118a2
israel <folaeazy0423@gmail.com>
2023-03-14 13:29:07 -0700

    modified

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,7 @@
 
 if (process.argv.length <= 3) {
   console.log(0);
-} else {
-  const list = process.argv.sort();
-  console.log(list.reverse()[1]);
+}else {
+   const list = process.argv.sort();
+   console.log(list.reverse()[1]);
 }
